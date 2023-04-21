94ad5ab97fe1fccdd45cc0568dd9460b337412ad
sethchens <bryan31032@gmail.com>
2023-04-20 20:27:37 -0600

    finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string f_Name = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string l_Name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {f_Name}, {f_Name} {l_Name}.");
     }
 }
