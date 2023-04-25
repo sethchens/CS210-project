aa5349e539c890d0e4e2d8cf78b15149633efcdf
sethchens <bryan31032@gmail.com>
2023-04-25 15:23:43 -0600

    commit prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numList = new List<int>();
+        int num;
+        int total;
+        double average;
+        int largest = 0;
+
+        Console.WriteLine("Enter a list of numbers; Tyoe 0 when finish.");
+
+        do {
+            num = int.Parse(Console.ReadLine());
+            numList.Add(num);
+            total = numList.Sum();
+
+            //Calculate the average but exclude the count of 0 
+            //to get the correct average number.
+
+            if (numList.Count != 1) {
+                average = total / (numList.Count - 1);
+            } else {
+                average = 0;
+            }
+            
+            if (num > largest) {
+                largest = num;
+            }
+        } while (num != 0);
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"THe average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
