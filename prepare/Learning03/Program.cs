0d7b71de34ff8e7a5b3d124c186012198ce67930
sethchens <bryan31032@gmail.com>
2023-05-17 20:17:21 -0600

    finished learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,51 +1,22 @@
 using System;
 
-class Program
+namespace Learning03
 {
-    public class Job {
-        public string _company;
-        public string _jobTitle;
-        public int _startYear;
-        public int _endYear;
-
-        public void Display() {
-            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-        }
-    }
-
-    public class Resume {
-        public string _name;
-        public List<Job> _jobs = new List<Job>();
-
-        public void Display() {
-            Console.WriteLine($"Name: {_name}");
-            Console.WriteLine("Jobs: ");
-            foreach (Job job in _jobs) {
-                job.Display();
-            }
-        }
-    }
-
-    static void Main(string[] args)
+    public class Program
     {
-        Job job1 = new Job();
-        job1._company = "Apple";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = 2022;
-        job1._endYear = 2023;
+        static void Main(string[] args)
+        {
+            Fraction defaultFraction = new Fraction();
+            Console.WriteLine(defaultFraction.GetFractionString());
 
-        Job job2 = new Job();
-        job2._jobTitle = "Software Engineer";
-        job2._company = "Microsoft";
-        job2._startYear = 2019;
-        job2._endYear = 2022;
+            Fraction secondFraction = new Fraction(5);
+            Console.WriteLine(secondFraction.GetFractionString());
 
-        Resume myResume = new Resume();
-        myResume._name = "Seth Chen";
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+            Fraction thirdFraction = new Fraction(3, 4);
+            Console.WriteLine(thirdFraction.GetFractionString());
 
-        myResume.Display();
-
+            Fraction forthFraction = new Fraction(5, 6);
+            Console.WriteLine(forthFraction.GetDecimalValue());
+        }
     }
 }
